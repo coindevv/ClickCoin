9d436f6c5b93146c5167fce6b6ec1bad051a149a
ClickCoinDev <winnaminakey@gmail.com>
2015-08-20 02:19:12 +0200

    First Commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define ClickCoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ClickCoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
