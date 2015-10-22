37f575744c44e120304649a1a11e776a83eb14c8
noise23 <admin@acidpool.com>
2015-10-23 00:42:52 +0300

    Delete unused COIN_YEAR_REWARD
    set RPC getmininginfo stakeinterest to 10 CLICK per block text

--- src/main.h
+++ src/main.h
@@ -39,7 +39,6 @@ static const unsigned int MAX_INV_SZ = 50000;
 static const int64_t MIN_TX_FEE = 1000;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 100000000 * COIN;
-static const int64_t COIN_YEAR_REWARD = 10 * CENT;
 static const int MODIFIER_INTERVAL_SWITCH = 1;
 
 inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
