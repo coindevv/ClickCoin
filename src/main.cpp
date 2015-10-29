428a959adf255e57bb10ad3d898a3a3f98ba0f36
noise23 <admin@acidpool.com>
2015-10-29 17:44:15 +0200

    Protocol version 85023
    Disconnect older versions on Mon, 16 Nov 2015 15:00:00 GMT

--- src/main.cpp
+++ src/main.cpp
@@ -2858,7 +2858,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
         CAddress addrFrom;
         uint64_t nNonce = 1;
         vRecv >> pfrom->nVersion >> pfrom->nServices >> nTime >> addrMe;
-        if (pfrom->nVersion < MIN_PEER_PROTO_VERSION)
+        if (nTime > 1447686000 && pfrom->nVersion < MIN_PEER_PROTO_VERSION)
         {
             // disconnect from peers older than this proto version
             printf("partner %s using obsolete version %i; disconnecting\n", pfrom->addr.ToString().c_str(), pfrom->nVersion);
